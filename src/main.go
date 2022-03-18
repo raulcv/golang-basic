c8ce39f3171ba0e83af651a14bad7194d4a09b79
Raul <iraulcv@gmail.com>
2022-03-18 02:21:21 -0500

    clean up

--- src/main.go
+++ src/main.go
@@ -2,28 +2,8 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/gorilla/mux"
 )
 
 func main() {
-	r := mux.NewRouter()
-	r.HandleFunc("/")
-	r.HandleFunc("/products")
-	m := make(map[string]int)
-	m["Raul"] = 14
-	m["Señor de Sipan"] = 15
-	fmt.Println(m)
-	//Record map value
-	for i, v := range m {
-		fmt.Println(i, v)
-	}
-	//Find a value into the map
-	valueFound, status := m["Raul"]
-	// fmt.Println(valueFound, i)
-	if status == false {
-		fmt.Println("value not Found")
-	} else {
-		fmt.Println(valueFound, status)
-	}
+	fmt.Println("HERE GOES ALL MAIN FUNCTIONS")
 }
