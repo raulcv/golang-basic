3b862cc5ccdf0dc66d8d41802ac3b3c6d96bec71
Raul <iraulcv@gmail.com>
2022-03-20 01:19:36 -0500

    Packages

---
+++ src/myownpkg/message.go
@@ -0,0 +1,10 @@
+package myownpkg
+
+import "fmt"
+
+func PrintMessage(msg string) {
+	fmt.Println(msg)
+}
+func PrintMessagePrivate(msg string) {
+	fmt.Println(msg)
+}
---
+++ src/race/car.go
@@ -0,0 +1,11 @@
+package race
+
+// CarPublic Car with public access
+type CarPublic struct {
+	Brand string
+	Year  int
+}
+type carPrivate struct {
+	Brand string
+	Year  int
+}
