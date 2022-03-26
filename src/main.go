a4c4b4dbd400b758960211ba740bd28474e52c96
Raul <iraulcv@gmail.com>
2022-03-26 15:48:59 -0500

    modified

--- src/main.go
+++ src/main.go
@@ -5,5 +5,14 @@ import (
 )
 
 func main() {
-	fmt.Println("HERE GOES ALL MAIN FUNCTIONS")
+
+	// e := echo.New()
+	// e.GET("/", func(c echo.Context) error {
+	// 	return c.String(http.StatusOK, "Hello, World!")
+	// })
+	// e.Logger.Fatal(e.Start(":3000"))
+	fmt.Println("Hello, World!")
+	var str string
+	var num int
+	fmt.Println(str, num)
 }
